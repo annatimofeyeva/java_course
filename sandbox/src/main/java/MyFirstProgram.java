7b8210adacbc0fc563f25e1c7e1e88856ac701c1
annatimofeyeva <annatimofeyeva@yandex.ru>
2016-02-28 16:35:27 -0800

    assignments 1 and 2 completed

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,8 @@
 public class MyFirstProgram {
-    public static void main (String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        Point p1 = new Point(0.0, 0.0);
+        Point p2 = new Point(3.0, 4.0);
+        System.out.println(Point.distance(p1, p2));
+        System.out.println(p1.distance(p2));
     }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,7 +2,24 @@
  * Created by IEUser on 2/21/2016.
  */
 public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double d2 = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+        return Math.sqrt(d2);
+
+    }
+
+    public double distance(Point p) {
+        double d2 = (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+        return Math.sqrt(d2);
 
 
-
+    }
 }
